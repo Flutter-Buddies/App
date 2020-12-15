288092433de5a592d0bd89f3f8740035e2f3d35d
joeyda3rd <joey.da3rd@gmail.com>
2020-12-14 20:36:03 -0500

    added comments

--- lib/widgets/user_widgets/user_widgets.dart
+++ lib/widgets/user_widgets/user_widgets.dart
@@ -1,6 +1,6 @@
 library user_widgets;
 
-// all included parts have the packages listed here available to them
+// all included parts (widgets) have the packages listed here available to them
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'widget_info.dart'; //defines the abstract info class
@@ -8,12 +8,17 @@ import 'widget_info.dart'; //defines the abstract info class
 // You need do the following to add your widget to the project
 // 1. Add "part of user_widgets" to your widget file.
 // 2. Add "part your_filename.dart" below.
-// 3. Extend WidgetInfo class in your widget file.
-// Note: You won't be able to import additional packages other than those declared here.
+// 3. Extend WidgetInfo class from 'widget_info.dart in your widget file.
+//   a. See test1.dart for an example
+// Note: You won't be able to import additional packages other than those declared here, except by permission.
+//       Initial version will only have built-in Flutter state management (setState(), InheritedWidget, etc)
+//       You can grow your widget as the app grows.
+//       This app WILL (hopefully) be published to the app stores and updated as new user widgets added!
 part 'test1.dart';
 
 //4. Add your Widget Info class to the list
 List<dynamic> widgetInfoList = [
   info,
+  // <-- add your widgetinfo class here
 ];
-// can this be done without adding the info to the list? (see user_widgets.dart)
+// can this be done without adding the widgetinfo to the list? (see user_widgets.dart)
