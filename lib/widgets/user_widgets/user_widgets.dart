27818cbc8ab56e21bcba7f66af372e2d9d3fa92f
joeyda3rd <joey.da3rd@gmail.com>
2020-12-15 13:02:58 -0500

    updated comments, added todos

--- lib/widgets/user_widgets/user_widgets.dart
+++ lib/widgets/user_widgets/user_widgets.dart
@@ -7,18 +7,20 @@ import 'widget_info.dart'; //defines the abstract info class
 
 // You need do the following to add your widget to the project
 // 1. Add "part of user_widgets" to your widget file.
-// 2. Add "part your_filename.dart" below.
+// 2. Add "part <your_filename>.dart" below.
 // 3. Extend WidgetInfo class from 'widget_info.dart in your widget file.
-//   a. See test1.dart for an example
+//   a. See example/example.dart for an example
 // Note: You won't be able to import additional packages other than those declared here, except by permission.
 //       Initial version will only have built-in Flutter state management (setState(), InheritedWidget, etc)
 //       You can grow your widget as the app grows.
 //       This app WILL (hopefully) be published to the app stores and updated as new user widgets added!
-part 'example.dart';
+part 'example/example.dart';
+
+ExampleInfo exampleInfo = ExampleInfo();
 
 //4. Add your Widget Info class to the list
 List<dynamic> widgetInfoList = [
-  info,
+  exampleInfo,
   // <-- add your widgetinfo class here
 ];
 // can this be done without adding the widgetinfo to the list? (see user_widgets.dart)
