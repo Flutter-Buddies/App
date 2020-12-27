fb16e3604fda2514b05649657ca060b989c7b2a4
joeyda3rd <joey.da3rd@gmail.com>
2020-12-27 13:15:56 -0500

    fixed duplicated imports

--- lib/modules/screens/home_screen.dart
+++ lib/modules/screens/home_screen.dart
@@ -2,8 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_buddies/widgets/user_widgets/user_widgets.dart';
-import 'package:flutter_buddies/widgets/user_widgets/widget_info.dart';
+//import 'package:flutter_buddies/widgets/user_widgets/user_widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
