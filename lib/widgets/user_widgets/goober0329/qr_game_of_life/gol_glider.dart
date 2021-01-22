ac48e00b223055ddd9f4bb4ea63d7c92a2a08bf9
Grayson Harrington <grayson.harrington@gmail.com>
2021-01-22 10:09:16 -0600

    fixed a bug in my glider animation where the glider didn't change states correctly.

--- lib/widgets/user_widgets/goober0329/qr_game_of_life/gol_glider.dart
+++ lib/widgets/user_widgets/goober0329/qr_game_of_life/gol_glider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_buddies/widgets/user_widgets/user_widgets.dart';
 
 /// Stateful Widget to control the glider animation
 /// that will go on the splash screen and loading screens
@@ -42,16 +43,16 @@ class GoLGlider extends StatefulWidget {
     ],
   ];
   final double gliderSize;
-  final double cornerRadiusRatio;
   final Duration duration;
   final Color color;
+  final double borderRadiusRatio;
 
   GoLGlider({
     @required this.gliderSize,
-    this.cornerRadiusRatio = 0,
-    this.color = Colors.black,
     this.duration = const Duration(milliseconds: 1500),
-  }) : assert(cornerRadiusRatio <= 1);
+    @required this.color,
+    @required this.borderRadiusRatio,
+  });
 
   @override
   _GoLGliderState createState() => _GoLGliderState();
@@ -60,43 +61,53 @@ class GoLGlider extends StatefulWidget {
 class _GoLGliderState extends State<GoLGlider>
     with SingleTickerProviderStateMixin {
   List<List<int>> modulePositions;
-  Container module;
   double moduleSize;
+  Container module;
 
   double gliderOffInit;
-  double gliderOff;
+  Animation gliderOff;
 
   int curState;
-  AnimationController _controller;
 
-  bool playing = false;
+  AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
-
     modulePositions = widget.modulePositions;
     moduleSize = widget.gliderSize / 5;
     module = Container(
-      width: moduleSize,
-      height: moduleSize,
+      width: widget.gliderSize / 5,
+      height: widget.gliderSize / 5,
       decoration: BoxDecoration(
         color: widget.color,
-        borderRadius:
-            BorderRadius.circular(widget.cornerRadiusRatio * moduleSize),
+        borderRadius: BorderRadius.circular(
+          widget.borderRadiusRatio * widget.gliderSize / 5,
+        ),
       ),
     );
 
     gliderOffInit = widget.gliderSize / 2 - moduleSize * 3 / 2;
-    gliderOff = gliderOffInit;
 
-    curState = 0;
+    curState = -1;
 
     _controller = AnimationController(
       vsync: this,
-      duration: widget.duration,
+      duration: Duration(milliseconds: widget.duration.inMilliseconds ~/ 4),
     );
+
+    gliderOff = Tween<double>(
+      begin: 0,
+      end: moduleSize / 4,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Interval(0, 1, curve: Curves.linear),
+      ),
+    );
+
     _controller.addListener(updateModulePositions);
+    _controller.forward();
   }
 
   @override
@@ -109,27 +120,19 @@ class _GoLGliderState extends State<GoLGlider>
   /// moves toward the top left corner based on the controller value, and the
   /// glider transitions through its four states during that period of time too.
   void updateModulePositions() {
-    if (playing) {
-      int newState = (_controller.value * 4).floor();
-      int nextIter = newState == 4 ? 1 : 0;
-      if (newState != curState && newState != 0) {
-        for (int i = 0; i < modulePositions.length; i++) {
-          modulePositions[i][0] +=
-              widget.modulePositionStateChange[newState - 1][i][0] - nextIter;
-          modulePositions[i][1] +=
-              widget.modulePositionStateChange[newState - 1][i][1] - nextIter;
-        }
-        curState = newState;
+    // TODO things are wrong
+    if (_controller.isCompleted) {
+      int oldState = curState;
+      curState = (curState + 1) % 4;
+      int nextIter = (curState == 0 && oldState == 3) ? 1 : 0;
+      for (int i = 0; i < modulePositions.length; i++) {
+        modulePositions[i][0] +=
+            widget.modulePositionStateChange[curState][i][0] - nextIter;
+        modulePositions[i][1] +=
+            widget.modulePositionStateChange[curState][i][1] - nextIter;
       }
-
-      setState(() {
-        gliderOff = gliderOffInit - _controller.value * moduleSize;
-      });
-
-      if (_controller.isCompleted) {
-        _controller.reset();
-        _controller.forward();
-      }
+      _controller.reset();
+      _controller.forward();
     }
   }
 
@@ -137,55 +140,62 @@ class _GoLGliderState extends State<GoLGlider>
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          width: widget.gliderSize,
-          height: widget.gliderSize,
-          child: Stack(
-            children: [
-              Positioned(
-                left: modulePositions[0][0] * moduleSize + gliderOff,
-                top: modulePositions[0][1] * moduleSize + gliderOff,
-                child: module,
+        AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            double gliderOffTrue =
+                gliderOffInit - gliderOff.value - curState * moduleSize / 4;
+            return Container(
+              width: widget.gliderSize,
+              height: widget.gliderSize,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: modulePositions[0][0] * moduleSize + gliderOffTrue,
+                    top: modulePositions[0][1] * moduleSize + gliderOffTrue,
+                    child: module,
+                  ),
+                  Positioned(
+                    left: modulePositions[1][0] * moduleSize + gliderOffTrue,
+                    top: modulePositions[1][1] * moduleSize + gliderOffTrue,
+                    child: module,
+                  ),
+                  Positioned(
+                    left: modulePositions[2][0] * moduleSize + gliderOffTrue,
+                    top: modulePositions[2][1] * moduleSize + gliderOffTrue,
+                    child: module,
+                  ),
+                  Positioned(
+                    left: modulePositions[3][0] * moduleSize + gliderOffTrue,
+                    top: modulePositions[3][1] * moduleSize + gliderOffTrue,
+                    child: module,
+                  ),
+                  Positioned(
+                    left: modulePositions[4][0] * moduleSize + gliderOffTrue,
+                    top: modulePositions[4][1] * moduleSize + gliderOffTrue,
+                    child: module,
+                  ),
+                ],
               ),
-              Positioned(
-                left: modulePositions[1][0] * moduleSize + gliderOff,
-                top: modulePositions[1][1] * moduleSize + gliderOff,
-                child: module,
-              ),
-              Positioned(
-                left: modulePositions[2][0] * moduleSize + gliderOff,
-                top: modulePositions[2][1] * moduleSize + gliderOff,
-                child: module,
-              ),
-              Positioned(
-                left: modulePositions[3][0] * moduleSize + gliderOff,
-                top: modulePositions[3][1] * moduleSize + gliderOff,
-                child: module,
-              ),
-              Positioned(
-                left: modulePositions[4][0] * moduleSize + gliderOff,
-                top: modulePositions[4][1] * moduleSize + gliderOff,
-                child: module,
-              ),
-            ],
-          ),
+            );
+          },
         ),
         IconButton(
           iconSize: 40,
           icon: Icon(
-            playing ? Icons.pause_circle_outline : Icons.play_circle_outline,
-            color: widget.color,
+            _controller.isAnimating
+                ? Icons.pause_circle_outline
+                : Icons.play_circle_outline,
+            color: golColor,
           ),
           onPressed: () {
-            if (playing) {
+            if (_controller.isAnimating) {
               setState(() {
                 _controller.stop();
-                playing = false;
               });
             } else {
               setState(() {
                 _controller.forward();
-                playing = true;
               });
             }
           },
