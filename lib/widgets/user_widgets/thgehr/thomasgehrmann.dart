9a17f3ba38e841e2a26a74bb8ba2fddd65bc522f
Thomas Gehrmann <thomas.gehrmann@rwth-aachen.de>
2021-06-05 22:52:55 +0200

    Add GehrmannWidget to project

---
+++ lib/widgets/user_widgets/thgehr/thomasgehrmann.dart
@@ -0,0 +1,41 @@
+part of user_widgets;
+
+//user defines from abstract class needed information
+class GehrmannInfo extends WidgetInfo {
+  @override
+  String get name =>
+      'My Widget'; //name your widget, not used in design currently
+
+  @override
+  String get description => 'This is an example of a basic widget';
+
+  @override
+  String get developer => 'Joey Marino'; //use any name for yourself
+
+  @override
+  String get logoPath => 'assets/profile_logos/flutter_buddy.png';
+
+  @override
+  Widget get widget => GehrmannWidget();
+}
+
+GehrmannInfo gehrmannInfo = GehrmannInfo();
+
+// User's widget lives here
+class GehrmannWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Center(
+              child: Text('Example Widget'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
